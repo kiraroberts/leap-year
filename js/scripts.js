766973508663fe93f021a8572d664cf04bc5f92c
Dominic Montelongo and Kira Roberts <dominic+kira@>
2019-06-13 08:51:52 -0700

    Add intial directories, html, front end logic, a little back end

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// BACK END
+
+var leapYear = function(year) {
+  return false;
+};
+
+// FRONT END
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
